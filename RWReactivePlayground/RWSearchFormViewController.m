0f9de6e19acbdbf78047d56b3e4614029d466754
lijia <lijia@huajiao.tv>
2017-11-09 14:34:53 +0800

    add tweet view into Main.storyboard

--- RWReactivePlayground/RWSearchFormViewController.m
+++ RWReactivePlayground/RWSearchFormViewController.m
@@ -6,15 +6,13 @@
 //  Copyright (c) 2013 Colin Eberhardt. All rights reserved.
 //
 
+#import <ReactiveObjC/ReactiveObjC.h>
 #import "RWSearchFormViewController.h"
 #import "RWSearchResultsViewController.h"
 
-@interface RWSearchFormViewController ()
-
+@interface RWSearchFormViewController () <UISplitViewControllerDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *searchText;
-
 @property (strong, nonatomic) RWSearchResultsViewController *resultsViewController;
-
 @end
 
 @implementation RWSearchFormViewController
@@ -27,8 +25,21 @@
   
   [self styleTextField:self.searchText];
   
-  self.resultsViewController = self.splitViewController.viewControllers[1];
+//  self.resultsViewController = self.splitViewController.viewControllers[1];
+  NSDictionary* attributes = @{NSForegroundColorAttributeName: [UIColor blackColor]};
+  self.navigationController.navigationBar.titleTextAttributes = attributes;
+  [self.navigationController.navigationBar setBackgroundImage:nil forBarMetrics:UIBarMetricsDefault];
+  self.navigationController.navigationBar.barTintColor = [UIColor whiteColor];
   
+  self.navigationController.splitViewController.delegate = self;
+  RAC(self.searchText, backgroundColor) = [self.searchText.rac_textSignal map:^id (NSString *value) {
+    return [self isValidSearchText:value] ? [UIColor whiteColor] : [UIColor yellowColor];
+  }];
+  
+}
+
+- (BOOL)isValidSearchText:(NSString*)text {
+  return text.length > 2;
 }
 
 - (void)styleTextField:(UITextField *)textField {

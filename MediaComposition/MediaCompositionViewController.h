00023908266d9baa70e69f40f6ad1ecc154c9ebd
lijia <lijia@huajiao.tv>
2018-01-04 11:47:20 +0800

    rename AVMediaBlend -> MediaComposition

---
+++ MediaComposition/MediaCompositionViewController.h
@@ -0,0 +1,13 @@
+//
+//  MediaCompositionViewController.h
+//  AVMediaBlend
+//
+//  Created by lijia on 2018/1/4.
+//  Copyright © 2018年 Colin Eberhardt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MediaCompositionViewController : UIViewController
+
+@end
---
+++ MediaComposition/MediaCompositionViewController.m
@@ -0,0 +1,22 @@
+//
+//  MediaCompositionViewController.m
+//  AVMediaBlend
+//
+//  Created by lijia on 2018/1/4.
+//  Copyright © 2018年 Colin Eberhardt. All rights reserved.
+//
+
+#import "MediaCompositionViewController.h"
+
+@interface MediaCompositionViewController ()
+
+@end
+
+@implementation MediaCompositionViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+@end

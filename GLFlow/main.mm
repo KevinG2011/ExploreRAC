b951e520dadbe05196a1a6e5e2baba06f4b5ca1b
lijia1 <iris.lance.jia@gmail.com>
2018-01-11 18:47:39 +0800

    main.m -> main.mm

--- GLFlow/main.mm
+++ GLFlow/main.mm
@@ -9,11 +9,11 @@
 #import <Foundation/Foundation.h>
 #include <glad/glad.h>
 #include <glfw3.h>
+#include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
@@ -24,26 +24,33 @@ int main(int argc, const char * argv[]) {
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
         
         GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
         if (window == NULL)
         {
+            std::cout << "Failed to create GLFW window" << std::endl;
             glfwTerminate();
             return -1;
         }
         glfwMakeContextCurrent(window);
         glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
         
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        {
+            std::cout << "Failed to initialize GLAD" << std::endl;
+            return -1;
+        }
+        
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
             glClear(GL_COLOR_BUFFER_BIT);
-            
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
-            
             /* Poll for and process events */
             glfwPollEvents();
         }
@@ -52,4 +59,9 @@ int main(int argc, const char * argv[]) {
 
     }
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }

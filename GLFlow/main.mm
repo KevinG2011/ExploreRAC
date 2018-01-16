d3347a60ea9dab21e851f0f5291b98868cc3a7b4
lijia <lijia@huajiao.tv>
2018-01-16 19:32:00 +0800

    more color attribute

--- GLFlow/main.mm
+++ GLFlow/main.mm
@@ -16,20 +16,20 @@ void processInput(GLFWwindow* window);
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
-    "out vec4 vertexColor;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 ourColor;\n"
     "void main()\n"
     "{\n"
-        "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-        "vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+        "gl_Position = vec4(aPos, 1.0);\n"
+        "ourColor = aColor;\n"
     "}\n\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
-    "in vec4 vertexColor;\n"
     "out vec4 FragColor;\n"
-    "uniform vec4 ourColor;\n"
+    "in vec3 ourColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = ourColor;\n"
+    "   FragColor = vec4(ourColor, 1.0);\n"
     "}\n\0";
 
 int main(int argc, const char * argv[]) {
@@ -110,9 +110,10 @@ int main(int argc, const char * argv[]) {
         // set up vertex data (and buffer(s)) and configure vertex attributes
         // ------------------------------------------------------------------
         float vertices[] = {
-            -0.5f, -0.5f, 0,    //left
-            0.5f, -0.5f, 0,    //right
-            0.0f,  0.5f, 0,    //top
+            //位置            //颜色
+             0.5f, -0.5f, 0, 1.0f, 0.0f, 0.0f,
+            -0.5f, -0.5f, 0, 0.0f, 1.0f, 0.0f,
+             0.0f,  0.5f, 0, 0.0f, 0.0f, 1.0f,
         };
     
         unsigned int VBO, VAO;
@@ -122,8 +123,11 @@ int main(int argc, const char * argv[]) {
         
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
         glEnableVertexAttribArray(0);
+        
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
         
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
@@ -133,7 +137,7 @@ int main(int argc, const char * argv[]) {
         glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
         std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
 #endif
-        
+
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
@@ -142,14 +146,14 @@ int main(int argc, const char * argv[]) {
             glClear(GL_COLOR_BUFFER_BIT);
             processInput(window);
             
-            float timeValue = glfwGetTime();
-            float greenValue = sin(timeValue) / 2.f + 0.5f;
-            int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-            if (vertexColorLocation != -1) {
-                /*set uniform must use program first */
-                glUseProgram(shaderProgram);
-                glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-            }
+//            float timeValue = glfwGetTime();
+//            float greenValue = sin(timeValue) / 2.f + 0.5f;
+//            int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+//            if (vertexColorLocation != -1) {
+//                /*set uniform must use program first */
+                    glUseProgram(shaderProgram);
+//                glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+//            }
             
             glBindVertexArray(VAO);
             glDrawArrays(GL_TRIANGLES, 0, 3);

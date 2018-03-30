8e8630f4d5e5e10b265536d112449295976f4b46
lijia <lijia@huajiao.tv>
2018-03-30 19:16:14 +0800

    add realm data manager

--- RWReactivePlayground/MVVMFlickr/Database/RWDatabaseManager.m
+++ RWReactivePlayground/MVVMFlickr/Database/RWDatabaseManager.m
@@ -23,26 +23,26 @@
 {
     self = [super init];
     if (self) {
-//        NSArray *docPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-//        NSString *path = [docPath objectAtIndex:0];
-//        NSString *databaseName = @"";
-//        NSString *filePath = [path stringByAppendingPathComponent:databaseName];
-//        NSLog(@"数据库目录 = %@",filePath);
-//
-//        RLMRealmConfiguration *config = [RLMRealmConfiguration defaultConfiguration];
-//        config.fileURL = [NSURL URLWithString:filePath];
+        NSArray *docPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString *path = [docPath objectAtIndex:0];
+        NSString *databaseName = @"";
+        NSString *filePath = [path stringByAppendingPathComponent:databaseName];
+        NSLog(@"数据库目录 = %@",filePath);
+
+        RLMRealmConfiguration *config = [RLMRealmConfiguration defaultConfiguration];
+        config.fileURL = [NSURL URLWithString:filePath];
 //        config.objectClasses = @[MyClass.class, MyOtherClass.class];
-//        config.readOnly = NO;
-//        int currentVersion = 1.0;
-//        config.schemaVersion = currentVersion;
-//
-//        config.migrationBlock = ^(RLMMigration *migration , uint64_t oldSchemaVersion) {
-//            // 这里是设置数据迁移的block
-//            if (oldSchemaVersion < currentVersion) {
-//            }
-//        };
-//
-//        [RLMRealmConfiguration setDefaultConfiguration:config];
+        config.readOnly = NO;
+        int currentVersion = 1.0;
+        config.schemaVersion = currentVersion;
+
+        config.migrationBlock = ^(RLMMigration *migration , uint64_t oldSchemaVersion) {
+            // 这里是设置数据迁移的block
+            if (oldSchemaVersion < currentVersion) {
+            }
+        };
+
+        [RLMRealmConfiguration setDefaultConfiguration:config];
     }
     return self;
 }

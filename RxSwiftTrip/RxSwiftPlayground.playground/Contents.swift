c339bfcd8ae4fcc8afecf2f66ee075121a8701f3
lijia1 <iris.lance.jia@gmail.com>
2018-06-06 07:54:14 +0800

    rxswift playground

--- RxSwiftTrip/RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftTrip/RxSwiftPlayground.playground/Contents.swift
@@ -5,3 +5,72 @@ import RxSwift
 
 var str = "Hello, playground"
 _ = Observable.of("Hello world!")
+let o1 = Observable.just(episodeI)
+
+let o2 = Observable.of(episodeII, episodeIII)
+let o3 = Observable.of([episodeII, episodeIV, episodeV])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public func example(of description: String, action: () -> Void) {
+  print("\n--- Example of:", description, "---")
+  action()
+}
+
+public let episodeI = "The Phantom Menace"
+public let episodeII = "Attack of the Clones"
+public let theCloneWars = "The Clone Wars"
+public let episodeIII = "Revenge of the Sith"
+public let solo = "Solo"
+public let rogueOne = "Rogue One"
+public let episodeIV = "A New Hope"
+public let episodeV = "The Empire Strikes Back"
+public let episodeVI = "Return Of The Jedi"
+public let episodeVII = "The Force Awakens"
+public let episodeVIII = "The Last Jedi"
+public let episodeIX = "Episode IX"
+

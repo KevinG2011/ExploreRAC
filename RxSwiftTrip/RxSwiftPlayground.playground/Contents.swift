2f361a1aa0d71b1399fe8af4bad0101b64faf66d
lijia1 <iris.lance.jia@gmail.com>
2018-06-13 09:23:26 +0800

    set up

--- RxSwiftTrip/RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftTrip/RxSwiftPlayground.playground/Contents.swift
@@ -76,7 +76,32 @@ example(of: "ReplaySubject") {
   quotes.onNext(episodeVIII);
 }
 
-
+example(of: "Dealt") {
+  let disposeBag = DisposeBag()
+  
+  let dealtHand = PublishSubject<[(String, Int)]>()
+  
+  func deal(_ cardCount: UInt) {
+    var deck = cards
+    var cardsRemaining: UInt32 = 52
+    var hand = [(String, Int)]()
+    
+    for _ in 0..<cardCount {
+      let randomIndex = Int(arc4random_uniform(cardsRemaining))
+      hand.append(deck[randomIndex])
+      deck.remove(at: randomIndex)
+      cardsRemaining -= 1
+    }
+    
+    // Add code to update dealtHand here
+    dealtHand
+  }
+  
+  // Add subscription to dealtHand here
+  
+  
+  deal(3)
+}
 
 
 

9b1a3d6d73385e741c415c773c25482eda4e325f
lijia1 <iris.lance.jia@gmail.com>
2018-06-13 08:50:46 +0800

    prepare for create a Blackjack Card Dealer.

--- RxSwiftTrip/RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftTrip/RxSwiftPlayground.playground/Contents.swift
@@ -3,33 +3,13 @@
 import UIKit
 import RxSwift
 
-public func example(of description: String, action: () -> Void) {
-  print("\n--- Example of:", description, "---")
-  action()
-}
-
-public let episodeI = "The Phantom Menace"
-public let episodeII = "Attack of the Clones"
-public let theCloneWars = "The Clone Wars"
-public let episodeIII = "Revenge of the Sith"
-public let solo = "Solo"
-public let rogueOne = "Rogue One"
-public let episodeIV = "A New Hope"
-public let episodeV = "The Empire Strikes Back"
-public let episodeVI = "Return Of The Jedi"
-public let episodeVII = "The Force Awakens"
-public let episodeVIII = "The Last Jedi"
-public let episodeIX = "Episode IX"
-
-
 let disposeBag = DisposeBag()
-
 
 example(of: "observable") {
   _ = Observable.of("Hello world!")
-  let o1 = Observable.just(episodeI)
-  let o2 = Observable.of(episodeII, episodeIII)
-  let o3 = Observable.of([episodeII, episodeIV, episodeV])
+  let _ = Observable.just(episodeI)
+  let _ = Observable.of(episodeII, episodeIII)
+  let _ = Observable.of([episodeII, episodeIV, episodeV])
 }
 
 example(of: "subscribe") {
@@ -94,9 +74,7 @@ example(of: "ReplaySubject") {
     .disposed(by: disposeBag)
   
   quotes.onNext(episodeVIII);
-  
 }
-
 
 
 
